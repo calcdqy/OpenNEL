44b5272a421a2ac5910767d90aedabc4f431a201
FandMC <winme2002@outlook.com>
2025-12-01 22:16:27 +0800

    md版本号又忘改了

--- OpenNEL/Type/AppInfo.cs
+++ OpenNEL/Type/AppInfo.cs
@@ -1,7 +1,7 @@
 namespace OpenNEL.type;
 class AppInfo
 {
-    public const string AppVersion = "1.0.7 - pre5";
+    public const string AppVersion = "1.0.7";
 
     public const string GithubUrL = "https://github.com/FandMC/OpenNEL";
     
